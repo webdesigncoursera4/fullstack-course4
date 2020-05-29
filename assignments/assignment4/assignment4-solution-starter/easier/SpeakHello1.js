9cd019522c77372a23eb1d49b9b272df1dfea739
webdesigncoursera4 <65479382+webdesigncoursera4@users.noreply.github.com>
2020-05-29 10:21:05 +0530

    Update and rename SpeakHello.js to SpeakHello1.js

---
+++ assignments/assignment4/assignment4-solution-starter/easier/SpeakHello1.js
@@ -0,0 +1,10 @@
+(function(window){
+  var hellospeaker={};
+  var speakWord = "Hello";
+ hellospeaker.speak=function (name) {
+  console.log(speakWord + " " + name);
+}
+ window.hellospeaker=hellospeaker;
+}(window);
+  
+
